dc5b18e54681e18cfcb7685d80afbe0565523c27
Christian Hagenest <mail@hagenest.dev>
2024-04-04 22:44:26 +0200

    codecs

--- configuration.nix
+++ configuration.nix
@@ -126,6 +126,20 @@
 
   environment.pathsToLink = [ "/share/zsh" ]; # for zsh completion
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
+
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
