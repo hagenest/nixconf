ea3cf19b5e58b55cd1b4b242e895c92cfb7f862f
Christian Hagenest <mail@hagenest.dev>
2024-04-05 15:08:42 +0200

    rename plasma6 declaration

--- configuration.nix
+++ configuration.nix
@@ -51,7 +51,7 @@
 
   # Enable the Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
